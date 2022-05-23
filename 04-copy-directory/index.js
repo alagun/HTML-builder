bf11530f84390d9d64dcde9baae7e6819e38b218
Alexey Lagun <alexey.lagun@gmail.com>
2022-05-24 02:06:28 +0300

    feat: add solution copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.readdir(path.join(__dirname, 'files'), 
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
+      if (err) throw err;
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    }); 
+  }
+);
