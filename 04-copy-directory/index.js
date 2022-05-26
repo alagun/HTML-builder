8e35798c0aaa24d5c4c63c8a4a7bf60722fdd9fd
Alexey Lagun <alexey.lagun@gmail.com>
2022-05-26 18:30:36 +0300

    fix: fixed files update

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(path.join(__dirname, 'files'), 
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) throw err;
-    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
-      if (err) throw err;
-      files.forEach(file => {
-        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
-          if (err) throw err;
-        });
-      });
-    }); 
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
+  if (err) {
+    return console.error(err);
   }
-);
+  fs.readdir(path.join(__dirname, 'files'), 
+    { withFileTypes: true },
+    (err, files) => {
+      if (err) throw err;
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, err => {
+        if (err) throw err;
+        files.forEach(file => {
+          fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+            if (err) throw err;
+          });
+        });
+      }); 
+    }
+  );
+});
